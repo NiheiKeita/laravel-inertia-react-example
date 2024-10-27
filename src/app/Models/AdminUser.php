db4a5c9144eda334ae053f713fd9d160c6409420
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2024-10-27 12:35:36 +0900

    STAN修正

--- src/app/Models/AdminUser.php
+++ src/app/Models/AdminUser.php
@@ -2,8 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -11,7 +9,6 @@ use Laravel\Sanctum\HasApiTokens;
 class AdminUser extends Authenticatable
 {
     use HasApiTokens;
-    use HasFactory;
     use Notifiable;
 
     /**
--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\DB;
@@ -11,7 +10,6 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens;
-    use HasFactory;
     use Notifiable;
 
     /**
