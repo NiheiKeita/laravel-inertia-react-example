abed570b4e13f5129cff5b7e2968efb35bfeb33c
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2026-06-19 07:55:38 +0900

    chore: update Storybook configuration and dependencies

--- src/vite.config.js
+++ src/vite.config.js
@@ -49,8 +49,7 @@ export default defineConfig({
           instances: [{
             browser: 'chromium'
           }]
-        },
-        setupFiles: ['.storybook/vitest.setup.ts']
+        }
       }
     }]
   }
